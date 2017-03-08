f4c1caa6f74615da3300593440a19b5c5718ae6a
IsuraManchanayake <isura.14@cse.mrt.ac.lk>
2017-03-08 21:57:47 +0530

    added point functions

--- src/biometricauthenticationscheme/PointXY.java
+++ src/biometricauthenticationscheme/PointXY.java
@@ -5,27 +5,50 @@
  */
 package biometricauthenticationscheme;
 
+import static java.lang.Math.sqrt;
+
 /**
  *
  * @author Isura Manchanayake
  */
 public class PointXY {
-    
-    private final double x;
-    private final double y;
-    
+
+    double x;
+    double y;
+
     public PointXY(double x, double y) {
         this.x = x;
         this.y = y;
     }
     
+    public PointXY(PointXY pt) {
+        this.x = pt.x;
+        this.y = pt.y;
+    }
+
     public PointXY() {
         this.x = 0.0;
         this.y = 0.0;
     }
-    
+
     @Override
     public String toString() {
-        return "(" + this.x + ", " + this.y + ")";
+        return this.x + " " + this.y;
+//        return "(" + this.x + ", " + this.y + ")";
+    }
+
+    public void setRelativeXY(PointXY origin) {
+        x -= origin.x;
+        y -= origin.y;
+    }
+    
+    public double distance(PointXY point) {
+        return sqrt((this.x - point.x) * (this.x - point.x) 
+                + (this.y - point.y) * (this.y - point.y));
+    }
+    
+    public void scale(double ratio) {
+        this.x *= ratio;
+        this.y *= ratio;
     }
 }
